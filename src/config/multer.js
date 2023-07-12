2f3febcda5cf4a80b85ff48e59aaa4a0603bd204
Eric-Coutinho <eric.coutinhop@hotmail.com>
2023-07-12 08:08:48 -0300

    Eita esqueci de fazer isso ontem kk

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,13 +1,17 @@
 const multer = require('multer');
 
 const multerConfig = multer.diskStorage({
+
     destination: (req, file, cb) => {
-        cb(null, 'public/img');
+    cb(null, 'public/img'); 
     },
+
     filename: (req, file, cb) => {
+    
         const fileName = `${new Date().getTime()}-${file.originalname}`;
-        cb(null, fileName);
+    
+        cb(null, fileName); 
     }
 });
 
-module.exports = { storage: multerConfig };
+module.exports = { storage:multerConfig };
